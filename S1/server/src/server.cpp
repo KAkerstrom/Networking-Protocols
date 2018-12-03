2e02674187cf66096054af0e90aedbdf9e5bdce6
Kyle Akerstrom <k.akerstrom@uleth.ca>
2018-12-03 08:43:59 -0700

    Wrote some server code. Wasn't able to test it at all, and it's probably incomplete anyway.

--- S1/server/src/server.cpp
+++ S1/server/src/server.cpp
@@ -1,8 +1,68 @@
 #include "ServerSocket.h"
 #include "SocketException.h"
 #include <string>
+#include <vector>
+#include "Exceptions.h"
 
 int main() {
+  std::cout << "running....\n";
+  try{
+    // Create the socket
+    ServerSocket server(3530);
+
+    while (true) {
+      ServerSocket new_sock;
+      server.accept(new_sock);
+
+      // For multiple threading, you need to create
+      // a new thread here and pass new_sock to it.
+      // The thread will use new_sock to communicate
+      // with the client.
+      try {
+        while (true) {
+          try {
+
+            Frame initialFrame;
+            do {
+              new_sock >> initialFrame;
+            }
+            while (initialFrame.getData() != "shunned_house.txt");
+
+            std::string filepath = "../../docs/shunned_house.txt";
+            std::vector<Packet> packets = PacketBuilder::getPacketsFromFile(filepath);
+
+            for (int p = 0; p < packets.size(); p++) {
+              std::vector<Frame> frames = packets[p].getFrames();
+              Frame response;
+
+              for (int f = 0; f < packets[i].getFrames().size(); f++)
+                do {
+                  new_sock << frames[f];
+                  new_sock >> response;
+                }
+                while (response.getFrameType() == Frame::NAK);
+            }
+          }
+          catch (file_io_error) {
+            std::cout << "Could not read file.\nExiting.\n";
+            return 0;
+          }
+
+         std::string data;
+         new_sock >> data;
+         new_sock << data;
+        }
+      }
+      catch(SocketException&) {}
+    }
+  }
+  catch (SocketException& e) {
+    std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
+  }
+  return 0;
+}
+
+/*int main() {
   std::cout << "running....\n";
   try{
     // Create the socket
@@ -30,4 +90,4 @@ int main() {
     std::cout << "Exception was caught:" << e.description() << "\nExiting.\n";
   }
   return 0;
-}
+}*/
